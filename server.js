b6a28b649ede6cf384d8cc0183b767fa72aa44c7
TheInfinity007 <ramchandra71999@gmail.com>
2020-08-02 22:36:06 +0530

    add the basic route

--- server.js
+++ server.js
@@ -3,12 +3,13 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+const dns = require('dns');
 var cors = require('cors');
+
 
 var app = express();
 
-// Basic Configuration 
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
@@ -18,8 +19,8 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
-
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
@@ -30,6 +31,21 @@ app.get('/', function(req, res){
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+app.post('/api/shorturl/new', (req, res)=>{
+	let url = req.body.url;
+	console.log(url);
+	var w3 = dns.lookup("https://www.w3schools.com", (err, address, family)=>{
+		if(err) console.log("The error is ", err);
+		else console.log(address, family);
+	})
+	console.log(w3);
+	res.json({
+		original_url: req.body.url,
+		short_url: 5
+	})
+	// res.send("HELLO INFINITY");
+})
 
 
 app.listen(port, function () {
